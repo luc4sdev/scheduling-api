6c724df318d18cfba2e61ca3ddef27f7aab2cf5c
lucas <lucas.pfranca01@gmail.com>
2026-01-09 22:40:13 -0300

    fix: configs for deploy

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { app } from '@/app';
+import { app } from '../app';
 import express from 'express';
 import { sequelize } from '@/config/database';
 import { env } from '@/env';
